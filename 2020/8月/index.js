0f35656a5634bde3614f485fe4b9933f82985406
罗辉 <huiluo@91jkys.com>
2020-09-25 11:44:53 +0800

    feat(add countnum)

---
+++ 2020/8月/index.js
@@ -0,0 +1,18 @@
+// 给定 nums = [2, 7, 11, 15], target = 9
+// 因为 nums[0] + nums[1] = 2 + 7 = 9
+// 所以返回 [0, 1]
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let i = nums.length;
+    while(i > 1) {
+        let last = nums.pop();
+        if (nums.indexOf(target - last) > -1) {
+            return [nums.indexOf(target - last), nums.length]
+        }
+        i--
+    }
+};
